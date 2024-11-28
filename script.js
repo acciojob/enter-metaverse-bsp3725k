3f6757546a1258b6d220b9968d4a0ef31013cf29
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-11-28 16:01:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 function textChange() {
 	let h1 = document.getElementById("status");
-	h1.innerHTML = "<h1>Enter the Metaverse</h1>"
+	h1.innerHTML = "<h1>Entered Metaverse</h1>"
 }
